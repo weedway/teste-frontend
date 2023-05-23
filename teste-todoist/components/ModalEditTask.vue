731f389a87a70241ca5d61eb11e58b3b292a93a1
chris-mafra <mafradesouza.cs@gmail.com>
2023-05-23 07:49:02 -0300

    feat: creat edit task modal

---
+++ teste-todoist/components/ModalEditTask.vue
@@ -0,0 +1,9 @@
+<template>
+<div id="editTaskModal" tabindex="-1" aria-hidden="true" class="fixed z-50 w-full p-4 overflow-x-hidden overflow-y-auto inset-0 h-screen max-h-full bg-black bg-opacity-60">
+    <div class="relative w-full max-w-2xl max-h-full mx-auto mt-10">
+        <div class="relative bg-white rounded-lg shadow ">
+            <TaskForm :type="'edit'"/>
+        </div>
+    </div>
+</div>
+</template>
